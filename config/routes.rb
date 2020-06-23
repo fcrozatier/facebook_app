f16128c5ba123a7ab76040bd4c10697d5d2c8abd
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-06-23 09:59:38 +0200

    Style User New and Edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
   devise_for :users
-  devise_scope :user do
-    get "", to: 'devise/registrations#new'
-  end
+  # devise_scope :user do
+  #   # get "", to: 'devise/registrations#new'
+  # end
 end
