1b2df1c09b93866c3228327f7406f53b535b8fb9
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-06-29 10:08:10 +0200

    Set friend_request a resource

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,10 +1,11 @@
 class FriendRequestsController < ApplicationController
   before_action :authenticate_user!
 
-  def create_friend_request
+  def create
     receiver = User.find(params[:receiver_id])
     FriendRequest.create(sender: current_user, receiver: receiver)
     receiver.notifications.create(family: "friend_request", who: current_user.id)
+    flash[:notice] = "Friend Request sent to #{receiver.name} !" 
     redirect_back(fallback_location: root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get '/', to: 'static_pages#index'
   get '/search', to: 'static_pages#search'
-  get '/friend_request', to: 'friend_requests#create_friend_request'
   devise_for :users
   resources :notifications, only: [:index]
+  resources :friend_requests, except: [:index]
   # devise_scope :user do
   #   # get "", to: 'devise/registrations#new'
   # end
