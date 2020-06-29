f6e2a6837161148993957a32b7cc1f0f2ecad6f2
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-06-29 11:55:58 +0200

    Create Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ApplicationRecord
   has_many :received_requests, class_name: "FriendRequest", dependent: :destroy, inverse_of: :receiver, foreign_key: "receiver_id"
   
   has_many :notifications
+  
+  has_many :sent_friendships, class_name: "Friendship", dependent: :destroy, inverse_of: :sender, foreign_key: "sender_id"
+  has_many :sent_friends, through: :sent_friendships, source: :receiver
+  has_many :received_friendships, class_name: "Friendship", dependent: :destroy, inverse_of: :receiver, foreign_key: "receiver_id"
+  has_many :received_friends, through: :received_friendships, source: :sender
+
+  def friends
+    (sent_friends + received_friends).uniq
+  end
 
   def new_notifications?
     notifications.where(viewed: false).count > 0
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_090231) do
+ActiveRecord::Schema.define(version: 2020_06_29_081625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_06_26_090231) do
   create_table "friend_requests", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "receiver_id"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
