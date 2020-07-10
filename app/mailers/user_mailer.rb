4e025969213805bc8f2e59b61113ceac59e8016e
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-07-10 12:15:06 +0200

    Use real from email adress

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "notification@facebook.com"
+  default from: "frederic.crozatier@protonmail.com"
 
   def welcome_email
     @user = params[:user]
--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,10 @@ Rails.application.initialize!
 
 # SendGrid
 ActionMailer::Base.smtp_settings = {
+  :from => 'frederic.crozatier@protonmail.com'
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'facebook.com',
+  :domain => 'https://afternoon-taiga-51893.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
