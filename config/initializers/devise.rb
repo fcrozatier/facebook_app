f619804beccb2a10ec358073d7c506a37fec5a68
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-07-10 10:43:39 +0200

    Move Google client id and secret key to credentials.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,8 +271,10 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, "836576752295-qnc3du03vjd5te6pps52queg95n19dv9.apps.googleusercontent.com",
-          "PhVDO6dT0wefJHvkquqTukI8", scope: 'userinfo.email,userinfo.profile' 
+  config.omniauth :google_oauth2, 
+      Rails.application.credentials.dig(:google, :google_client_id),
+      Rails.application.credentials.dig(:google, :google_secret_key),
+      scope: 'userinfo.email,userinfo.profile' 
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
